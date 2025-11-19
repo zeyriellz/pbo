a99d78c07461f192a95bc23fc0b27ca5cef6aacc
netyst <pineapplekuu@gmail.com>
2025-11-19 20:58:20 +0700

    .

--- Praktikum11/Demo.java
+++ Praktikum11/Demo.java
@@ -3,6 +3,14 @@ import java.util.ArrayList;
 public class Demo {
     public static void main(String[] args) {
         Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.Kom", "199402");
+        
+        Pegawai pegawai1 = dosen1;
+
+        System.out.println(pegawai1.nip);
+        System.out.println(pegawai1.nama);
+        System.out.println(pegawai1.nidn);
+        pegawai1.mengajar();
+        
         Dosen dosen2 = new Dosen("19700105", "Muhammad, S.T, M.T", "197001");
         TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", "Aida, A.Md.", "Tenaga Administrasi");
         TenagaKependidikan tendik2 = new TenagaKependidikan("19650304", "Rika, S.T.", "Tenaga Laboratorium");
