6759d42b3f54007cd301e8a4f7a3c76d9c25179a
netyst <pineapplekuu@gmail.com>
2025-11-19 21:32:29 +0700

    .

--- Praktikum11/Demo.java
+++ Praktikum11/Demo.java
@@ -1,33 +1,37 @@
-import java.util.ArrayList;
+// import java.util.ArrayList;
 
 public class Demo {
     public static void main(String[] args) {
         Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.Kom", "199402");
-        
-        Pegawai pegawai1 = dosen1;
-
-        System.out.println(pegawai1.nip);
-        System.out.println(pegawai1.nama);
-        pegawai1.displayInfo();
-
-        Dosen newDosen = (Dosen) pegawai1;
-
-        System.out.println(newDosen.nama);
-        System.out.println(newDosen.nidn);
-        newDosen.mengajar();
-        
-        Dosen dosen2 = new Dosen("19700105", "Muhammad, S.T, M.T", "197001");
         TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", "Aida, A.Md.", "Tenaga Administrasi");
-        TenagaKependidikan tendik2 = new TenagaKependidikan("19650304", "Rika, S.T.", "Tenaga Laboratorium");
+        
+        train(dosen1);
+        train(tendik1);
+        
+        // Pegawai pegawai1 = dosen1;
 
-        ArrayList<Pegawai> daftarPegawai = new ArrayList<Pegawai>();
+        // System.out.println(pegawai1.nip);
+        // System.out.println(pegawai1.nama);
+        // pegawai1.displayInfo();
 
-        daftarPegawai.add(dosen1);
-        daftarPegawai.add(dosen2);
-        daftarPegawai.add(tendik1);
-        daftarPegawai.add(tendik2);
+        // Dosen newDosen = (Dosen) pegawai1;
 
-        System.out.println("Jumlah Pegawai: " + daftarPegawai.size());
+        // System.out.println(newDosen.nama);
+        // System.out.println(newDosen.nidn);
+        // newDosen.mengajar();
+        
+        // Dosen dosen2 = new Dosen("19700105", "Muhammad, S.T, M.T", "197001");
+        // TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", "Aida, A.Md.", "Tenaga Administrasi");
+        // TenagaKependidikan tendik2 = new TenagaKependidikan("19650304", "Rika, S.T.", "Tenaga Laboratorium");
+
+        // ArrayList<Pegawai> daftarPegawai = new ArrayList<Pegawai>();
+
+        // daftarPegawai.add(dosen1);
+        // daftarPegawai.add(dosen2);
+        // daftarPegawai.add(tendik1);
+        // daftarPegawai.add(tendik2);
+
+        // System.out.println("Jumlah Pegawai: " + daftarPegawai.size());
         }
 
         public static void train(Pegawai pegawai) {
