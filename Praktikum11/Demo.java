d5dd47183c7d9d75e32a5b8ef859d81e5b10387c
netyst <pineapplekuu@gmail.com>
2025-11-26 22:52:04 +0700

    .

--- Praktikum11/Demo.java
+++ Praktikum11/Demo.java
@@ -1,5 +1,3 @@
-// import java.util.ArrayList;
-
 public class Demo {
     public static void main(String[] args) {
         Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom. M.Kom", "199402");
@@ -7,40 +5,22 @@ public class Demo {
         
         train(dosen1);
         train(tendik1);
-        
-        // Pegawai pegawai1 = dosen1;
-
-        // System.out.println(pegawai1.nip);
-        // System.out.println(pegawai1.nama);
-        // pegawai1.displayInfo();
-
-        // Dosen newDosen = (Dosen) pegawai1;
-
-        // System.out.println(newDosen.nama);
-        // System.out.println(newDosen.nidn);
-        // newDosen.mengajar();
-        
-        // Dosen dosen2 = new Dosen("19700105", "Muhammad, S.T, M.T", "197001");
-        // TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", "Aida, A.Md.", "Tenaga Administrasi");
-        // TenagaKependidikan tendik2 = new TenagaKependidikan("19650304", "Rika, S.T.", "Tenaga Laboratorium");
-
-        // ArrayList<Pegawai> daftarPegawai = new ArrayList<Pegawai>();
-
-        // daftarPegawai.add(dosen1);
-        // daftarPegawai.add(dosen2);
-        // daftarPegawai.add(tendik1);
-        // daftarPegawai.add(tendik2);
-
-        // System.out.println("Jumlah Pegawai: " + daftarPegawai.size());
-        }
+    }
 
         public static void train(Pegawai pegawai) {
             System.out.println("Memberikan pelatihan untuk pegawai");
             pegawai.displayInfo();
+            System.out.println("Mengenalkan lingkungan kampus");
+            System.out.println("Menginfokan SOP/Juknis");
 
-            //hanya test
-            System.out.println(pegawai.nidn);
-            System.out.println(pegawai.kategori);
-            pegawai.mengajar();
+            if (pegawai instanceof Dosen) {
+                Dosen newDosen = (Dosen) pegawai;
+                System.out.println("Memberikan pelatihan pedagogik");
+                newDosen.mengajar();
+            }
+
+            if (pegawai instanceof TenagaKependidikan) {
+                TenagaKependidikan newTenagaKependidikan = (TenagaKependidikan) pegawai;
+            }
         }
-    }
+}
